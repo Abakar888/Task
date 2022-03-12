6e422b54ec174b3b998561eafabea2a4150bdb97
Abakar <akap302@gmail.com>
2022-03-13 02:14:02 +0300

    class DataBase

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+class DataBase:
+    def __init__(self, items):
+        self.items = items
+
+    def write_data(self, element):
+        self.items.append(element)
+
+
 class Data:
     def __init__(self, country, name, age, gender, height, weight):
         self.country = country
@@ -6,3 +14,4 @@ class Data:
         self.gender = gender
         self.height = height
         self.weight = weight
+
