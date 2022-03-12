8ce3b0917f250c8a136698b40a7fa0013229d03e
Abakar <akap302@gmail.com>
2022-03-13 02:06:13 +0300

    task class

---
+++ main.py
@@ -0,0 +1,8 @@
+class Data:
+    def __init__(self, country, name, age, gender, height, weight):
+        self.country = country
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.height = height
+        self.weight = weight
